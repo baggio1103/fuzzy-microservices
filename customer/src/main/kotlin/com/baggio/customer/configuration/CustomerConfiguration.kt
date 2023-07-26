791fd590a890bb33f35755c00cf70051f3fe4e56
baggio1103 <valijanov1103@mail.ru>
2023-07-26 19:40:58 +0300

    Add new module clients - module for inter-service communication via loadBalanced web client using HttpExchange interface

--- customer/src/main/kotlin/com/baggio/customer/configuration/CustomerConfiguration.kt
+++ customer/src/main/kotlin/com/baggio/customer/configuration/CustomerConfiguration.kt
@@ -1,15 +1,31 @@
 package com.baggio.customer.configuration
 
+import com.baggio.clients.fraud.FraudService
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.support.WebClientAdapter
+import org.springframework.web.service.invoker.HttpServiceProxyFactory
 
 @Configuration
 class CustomerConfiguration {
 
     @Bean
     @LoadBalanced
-    fun restTemplate(): RestTemplate = RestTemplate()
+    fun webClient(): WebClient.Builder {
+        return WebClient.builder()
+    }
+
+    @Bean
+    fun fraudService(webClient: WebClient.Builder): FraudService {
+        val httpServiceProxyFactory = HttpServiceProxyFactory
+            .builder(WebClientAdapter.forClient(webClient
+                .apply { it.baseUrl("http://FRAUD") }
+                .build()))
+            .build()
+        return httpServiceProxyFactory.createClient(FraudService::class.java)
+    }
+
 
 }
