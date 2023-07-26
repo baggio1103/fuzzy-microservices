62ed5e012f1348a8554ae7390014c700f7c8093b
baggio1103 <valijanov1103@mail.ru>
2023-07-27 00:02:24 +0300

    Add api-gateway service to manage all request via single entry point for all services

---
+++ api-gateway/src/main/kotlin/com/baggio/ApiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.baggio
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApiGatewayApplication
+
+fun main() {
+    SpringApplication.run(ApiGatewayApplication::class.java)
+}
