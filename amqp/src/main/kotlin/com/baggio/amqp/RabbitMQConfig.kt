b7326ea2aea0d39c91769d2574d0fe99649fdeb9
baggio1103 <valijanov1103@mail.ru>
2023-08-05 15:38:15 +0500

    Remove unnecessary application.yml and explicitly specify service host

--- amqp/src/main/kotlin/com/baggio/amqp/RabbitMQConfig.kt
+++ amqp/src/main/kotlin/com/baggio/amqp/RabbitMQConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -16,11 +15,9 @@ import org.springframework.context.annotation.Primary
 class RabbitMQConfig {
 
     @Bean
-    fun connectionFactory(
-        @Value("\${service.name}") serviceName: String
-    ): ConnectionFactory {
+    fun connectionFactory(): ConnectionFactory {
         return CachingConnectionFactory().also {
-            it.setHost(serviceName)
+            it.setHost("rabbitmq")
         }
     }
 
